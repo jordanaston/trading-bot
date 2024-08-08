93713aae2ddd9cdc29a6b6d7029ffffd3b3f5880
Jordan Aston <jordanaston93@gmail.com>
2024-08-08 20:07:33 +1000

    initial commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center mt-40">
+      <div>
+        <p>Trading Bot</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
