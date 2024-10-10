b75e26e3ba117fcf45db085efaba100f654521aa
Jordan Aston <jordanaston93@gmail.com>
2024-10-10 12:35:31 +1000

    bot data in db

--- express-app/src/handlers/tradingView/tradingViewWebhook.ts
+++ express-app/src/handlers/tradingView/tradingViewWebhook.ts
@@ -1,43 +1,57 @@
 import { Request, Response } from "express";
 import { enterBuy } from "../../helpers/binance/enterBuy";
 import { enterSell } from "../../helpers/binance/enterSell";
-import { getBotStatus } from "../../helpers/binance/betBotStatus";
+import Bot from "../../models/Bot";
 
 export enum OrderSide {
   BUY = "BUY",
   SELL = "SELL",
 }
 
-let buyCount = 0;
-let lastTrade: OrderSide | null = null;
-
 export const tradingViewWebhook = async (req: Request, res: Response) => {
   try {
     const { symbol, side, testOrder } = req.body;
 
-    const botStatusActive = await getBotStatus();
+    const botData = (await Bot.findOne()) || {
+      buyCount: 0,
+      lastTrade: null,
+      active: true,
+    };
+    let { buyCount, lastTrade, active: botStatusActive } = botData;
+
+    console.log("BOT DATA: ", botData);
+
+    console.log("REQUEST BODY: ", req.body, "BUY COUNT: ", buyCount);
 
     if (!symbol || !side) {
+      console.log("Invalid request data.");
       return res.status(400).json({ message: "Invalid request data." });
     }
 
     if (botStatusActive === false) {
+      console.log("Bot is inactive.");
       return res.status(200).json({ message: "Bot is inactive." });
     }
 
     if (side === OrderSide.BUY) {
-      if (buyCount >= 5) {
+      if (Number(buyCount) >= 5) {
         const maxBuysMessage =
           "Maximum number of buys reached. Waiting for SELL alert...";
-        console.log(maxBuysMessage);
+        console.log(maxBuysMessage, "BUY COUNT: ", buyCount);
         return res.status(200).json({ message: maxBuysMessage });
       }
 
-      const buySuccess = await enterBuy(symbol, buyCount, testOrder);
+      const buySuccess = await enterBuy(symbol, Number(buyCount), testOrder);
+
+      if (buySuccess?.status === "FILLED") {
+        console.log("BUY FILLED");
+      }
 
       if (buySuccess && testOrder !== true) {
-        buyCount++;
+        buyCount = Number(buyCount) + 1;
         lastTrade = OrderSide.BUY;
+        await Bot.updateOne({}, { buyCount, lastTrade });
+        console.log("BUY COUNT + LAST TRADE: ", buyCount, lastTrade);
       }
     } else if (side === OrderSide.SELL) {
       if (lastTrade === OrderSide.SELL) {
@@ -49,12 +63,18 @@ export const tradingViewWebhook = async (req: Request, res: Response) => {
 
       const sellSuccess = await enterSell(symbol, testOrder);
 
+      if (sellSuccess?.status === "FILLED") {
+        console.log("SELL FILLED");
+      }
+
       if (sellSuccess && testOrder !== true) {
         buyCount = 0;
         lastTrade = OrderSide.SELL;
+        await Bot.updateOne({}, { buyCount, lastTrade });
+        console.log("BUY COUNT + LAST TRADE: ", buyCount, lastTrade);
       }
     }
-
+    console.log("Webhook triggered successfully");
     res.status(200).json({ message: "Webhook triggered successfully" });
   } catch (error: unknown) {
     if (error instanceof Error) {
--- express-app/src/models/Bot.ts
+++ express-app/src/models/Bot.ts
@@ -2,6 +2,8 @@ import mongoose, { Schema } from "mongoose";
 
 const botSchema: Schema = new Schema({
   active: { type: Boolean, required: true },
+  buyCount: { type: Number, required: true },
+  lastTrade: { type: String, required: true },
 });
 
 const Bot = mongoose.model("Bot", botSchema);
